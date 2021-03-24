5d702524304769f328fe7db9e27435c37eca78bf
Pradip-p <thapapradip542@gmail.com>
2021-03-24 13:38:28 +0545

    completed

--- Disease/settings.py
+++ Disease/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'Disease.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
-}
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',

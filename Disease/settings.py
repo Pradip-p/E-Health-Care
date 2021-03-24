d07f7ee49c4f2cfd3c0f5c77dbe43f5082fa4e13
Pradip-p <thapapradip542@gmail.com>
2021-03-24 13:35:20 +0545

    complted

--- Disease/settings.py
+++ Disease/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from .local_email import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,25 +83,25 @@ WSGI_APPLICATION = 'Disease.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'OPTIONS': {
-#           'autocommit': True,
-#             },
-#         'NAME': 'ehealth',
-#         'USER': 'root',
-#         'PASSWORD': 'Password_1',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
-# }
+}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+          'autocommit': True,
+            },
+        'NAME': 'ehealth',
+        'USER': 'root',
+        'PASSWORD': 'Password_1',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
--- doctor/urls.py
+++ doctor/urls.py
@@ -4,9 +4,9 @@ from doctor import views
 from django.views.generic.base import RedirectView
 
 urlpatterns = [        
-#path('doctor/',RedirectView.as_view(url='doctor_login/')),
+path('doctor/',RedirectView.as_view(url='doctor_login/')),
 path('doctor_login/', views.doctor_login,name='doctor_login'),
-# path('doctor_register/', views.doctor_register,name='doctor_register'),
+path('prescription/', views.prescription,name='prescription'),
 
 path('dashboard_doctor/', views.dashboard_doctor, name="dashboard_doctor"),
 path('doctor_logout/', views.doctor_logout, name="doctor_logout"),

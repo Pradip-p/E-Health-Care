8e66d75573364fdb7ebe3305e1bb4c534586efcf
Pradip-p <thapapradip542@gmail.com>
2021-04-01 22:42:47 +0545

    completed

--- Disease/settings.py
+++ Disease/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 
@@ -176,7 +176,7 @@ STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'patient/static')
 ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

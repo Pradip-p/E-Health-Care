6ec8c691fa53786b3ca461619603a2788ce0308b
Pradip-p <thapapradip542@gmail.com>
2021-04-02 12:40:39 +0545

    deploy

--- Disease/settings.py
+++ Disease/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # DEBUG = True
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-# ALLOWED_HOSTS = ['127.0.0.1']
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "128.199.29.4,localhost").split(",")
+ALLOWED_HOSTS = ['127.0.0.1']
+# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "128.199.29.4,localhost").split(",")
 # AUTH_USER_MODEL = 'core.User'
 
 #Development Mode
@@ -97,13 +97,13 @@ WSGI_APPLICATION = 'Disease.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 
-if DEVELOPMENT_MODE is True:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
-    }
+# if DEVELOPMENT_MODE is True:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#         }
+#     }
 # elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
 #     if os.getenv("DATABASE_URL", None) is None:
 #         raise Exception("DATABASE_URL environment variable not defined")
@@ -111,12 +111,12 @@ if DEVELOPMENT_MODE is True:
 #         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
 #     }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -176,7 +176,7 @@ STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'patient/static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

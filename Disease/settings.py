ac82dc34bb36943f72f91123bf0ae6071779ddce
Pradip-p <thapapradip542@gmail.com>
2021-04-03 10:59:33 +0545

    manage static files for deploy

--- Disease/settings.py
+++ Disease/settings.py
@@ -173,10 +173,10 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, 'patient/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

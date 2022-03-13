457353fd2dac078512aa3675c06c42a798e3c49a
Pradip-p <thapapradip542@gmail.com>
2022-03-13 10:44:47 +0545

    E- Health care

--- Disease/settings.py
+++ Disease/settings.py
@@ -90,25 +90,25 @@ WSGI_APPLICATION = 'Disease.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-          'autocommit': True,
-            },
-        'NAME': 'ehealth',
-        'USER': 'root',
-        'PASSWORD': 'Password_1',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#           'autocommit': True,
+#             },
+#         'NAME': 'ehealth',
+#         'USER': 'root',
+#         'PASSWORD': 'Password_1',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators

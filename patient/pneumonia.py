541f252a2cdf9e872fdea8107d20821df9d5581a
Pradip-p <thapapradip542@gmail.com>
2024-07-13 20:24:51 +0545

    fix(keras):  fixed import error from tensorflow

--- patient/pneumonia.py
+++ patient/pneumonia.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 from keras.preprocessing import image
 import zipfile
